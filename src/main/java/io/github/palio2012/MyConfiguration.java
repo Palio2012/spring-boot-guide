a9b1457b59216c049e21dcc09c108386512e498f
Palio2012 <daniasrayan.com@gmail.com>
2022-10-31 15:50:53 -0300

    Working with different profile properties and annotation @Profile

--- src/main/java/io/github/palio2012/MyConfiguration.java
+++ src/main/java/io/github/palio2012/MyConfiguration.java
@@ -1,10 +1,18 @@
 package io.github.palio2012;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
 
-
+    @Bean
+    public CommandLineRunner execute () {
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 
 }
