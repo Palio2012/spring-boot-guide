e531697ef78519c725207e30e8b0306a8c9df5ec
Palio2012 <daniasrayan.com@gmail.com>
2022-10-31 15:19:07 -0300

    application.properties file created

--- src/main/java/io/github/palio2012/MyConfiguration.java
+++ src/main/java/io/github/palio2012/MyConfiguration.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyConfiguration {
 
-    @Bean (name = "applicationName")
-    public String applicationName () {
-        return "Sistema de Vendas";
-    }
+
 
 }
